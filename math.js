4e4c1b1e14ab10f753524a8e5fdb3a680dc72236
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-03-18 21:27:23 +0530

    Initial commit

---
+++ math.js
@@ -0,0 +1,17 @@
+// Generate Number between 10 and 20
+
+
+const Min = 1;
+const Max = 3;
+
+
+console.log(Math.floor(Math.random() * (Max - Min + 1)) + Min);
+
+// Uses for Rock, Paper, Scissors game //
+const choices = ["Rock", "Paper", "Scissors", "test"];
+const computerChoice = choices[Math.floor(Math.random() * 3)];
+console.log(computerChoice);
+
+
+console.log(Math.floor(Math.random() * 3));
+
