b158f16c816b2e14639f29cb5b1ccd53b4ee6692
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-04-29 21:12:59 +0530

    variable

---
+++ 2-variable/1-variable.js
@@ -0,0 +1,34 @@
+// variable = A container that stores a value.
+//              Behaves as if it were the value it contains.
+
+
+// Two steps to create a varaible
+// 1. Declaration let x;
+// 2. Assignment x = 100;
+
+// Or we can declare and assign together
+// let a = 2;
+
+// Numbers 
+
+let a = 100;
+console.log(a);
+console.log(`The assignet number to 'a' is ${a}`);
+
+// Strings
+
+let firstName = "Sandesh";
+console.log(firstName);
+console.log(`The user first name is ${firstName}`);
+
+let lastName = 'Shrestha'
+console.log(`The user first name is ${firstName} and last name is ${lastName}`);
+
+// Booleans - Either True or False
+
+let online = true;
+console.log(online);
+
+let forSale = false;
+console.log(forSale);
+
