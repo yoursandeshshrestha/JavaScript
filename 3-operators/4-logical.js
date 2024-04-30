771b1509ef04bcdac01f122439bf9491ec3f1830
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-04-30 20:34:45 +0530

    logical operators

---
+++ 3-operators/4-logical.js
@@ -0,0 +1,13 @@
+// ! Logical Operators:
+
+  // ?  AND (&&): Returns true if both operands are true.
+    Example: 
+    (5 > 3) && (4 < 7) // returns true.
+    
+  // ?  OR (||): Returns true if either operand is true.
+    Example: 
+    (5 < 3) || (4 < 7) // returns true.
+    
+  // ? NOT (!): Returns the opposite of the operand.
+    Example: 
+    !(5 == 3) // returns true.
