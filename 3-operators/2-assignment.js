c25d2c41f92ccadb3c688e3ab0a09f9886d08a66
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-04-30 20:34:04 +0530

    assignment

---
+++ 3-operators/2-assignment.js
@@ -0,0 +1,52 @@
+// ? Assignment (=): 
+// This operator assigns the value on the 
+// right-hand side to the variable on the left-hand side.
+
+let x = 5; // Assigns the value 5 to the variable x
+let personName = "Sandesh";
+
+// ? Addition Assignment (+=): 
+// This operator adds the value on 
+// the right-hand side to the variable on the left-hand side and 
+// assigns the result to the variable.
+
+let a = 5;
+a += 3; // Equivalent to: a = a + 3; (results in x being 8)
+
+// ? Subtraction Assignment (-=): 
+// This operator subtracts the value 
+// on the right-hand side from the variable on the left-hand side 
+// and assigns the result to the variable.
+
+let b = 5;
+b -= 3; // Equivalent to: b = b - 3; (results in x being 2)
+
+// ? Multiplication Assignment (*=): 
+// This operator multiplies the value 
+// on the right-hand side with the variable on the left-hand side and 
+// assigns the result to the variable.
+
+let c = 5;
+c *= 3; // Equivalent to: c = c * 3; (results in x being 15)
+
+// ? Division Assignment (/=): 
+// This operator divides the variable on the left-hand side by the 
+// value on the right-hand side and assigns the result to the variable.
+
+let d = 10;
+d /= 2; // Equivalent to: d = d / 2; (results in x being 5)
+
+// ? Modulus Assignment (%=): 
+// This operator divides the variable on the left-hand side by the value 
+// on the right-hand side and assigns the remainder to the variable.
+
+let e = 10;
+e %= 3; // Equivalent to: e = e % 3; (results in x being 1)
+
+// ? Exponentiation Assignment (**=): 
+// This operator raises the variable on the left-hand side to the power 
+// of the value on the right-hand side and assigns the result to the variable.
+
+let f = 2;
+f **= 3; // Equivalent to: f = f ** 3; (results in x being 8)
+
