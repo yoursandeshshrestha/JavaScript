060f4761d16aa65a8fcb6d39e14bfa06534056d1
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-04-30 20:33:54 +0530

    arithmetic

---
+++ 3-operators/1-arithmetic.js
@@ -0,0 +1,24 @@
+// ? arithmetic operators = operands (values, variables, etc.)
+// ?                                       operators (+ - * /)
+// ?                                      ex. 11 = x + 5;
+
+
+// ! Addition (+): Adds two operands.
+let sum = 5 + 3; 
+console.log(sum); // sum = 8
+
+// ! Subtraction (-): Subtracts the second operand from the first.
+let difference = 10 - 4; 
+console.log(difference); // difference = 6
+
+// ! Multiplication (*): Multiplies two operands.
+let product = 2 * 6; 
+console.log(product); // product = 12
+
+// ! Division (/): Divides the first operand by the second.
+let quotient = 8 / 2; 
+console.log(quotient); // quotient = 4
+
+// ! Modulus (%): Returns the remainder of the division operation.
+let remainder = 10 % 3; 
+console.log(remainder); // remainder = 1
