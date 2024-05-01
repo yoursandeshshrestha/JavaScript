a69734f1fabedc79691dbcecf225bff70891d047
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-01 22:58:23 +0530

    elseif

---
+++ 4-conditionalflow/2-elseif.js
@@ -0,0 +1,9 @@
+let x = 12;
+
+if (x > 10) {
+    console.log(`${x} is greater than 10`);
+} else if (x === 10) {
+    console.log(`${x} is equal to 10`);
+} else if (x < 10) {
+    console.log(`${x} is smaller to 10`);
+}
