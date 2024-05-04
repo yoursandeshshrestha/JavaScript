caf08f361ab28a493be7b7ed964a806a17673db2
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-04 22:50:36 +0530

    function expression

---
+++ 6-functions/2-functionexpression.js
@@ -0,0 +1,13 @@
+// Anonymous function expression
+let greet = function(name) {
+    console.log(`Hello, ${name}`);
+};
+
+greet("Sandesh"); // Output: Hello, Bob!
+
+// Named function expression
+let multiply = function multiply(a, b) {
+    return a * b;
+};
+
+console.log(multiply(2, 3)); // Output: 6
