8de43572a40c25321d6403ff0857fb709719c7fa
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-04 22:50:45 +0530

    arrow function

---
+++ 6-functions/3-arrowfunction.js
@@ -0,0 +1,16 @@
+// Normal Function
+
+function greet(name){
+    console.log(`Hello, ${name}`);
+}
+
+greet("Sandesh")
+
+// Arrow Function
+
+let message =  (userinput, age) => {
+    console.log(`Hello ${userinput}`)
+    console.log(`You are ${age}, year's old`)
+}
+
+message("jasmine", 20);
