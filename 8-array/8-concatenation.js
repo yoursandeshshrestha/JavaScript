79b9d9b1ce4e17daca1ad488783f50908f4ab701
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-09 23:57:43 +0530

    concatenation

---
+++ 8-array/8-concatenation.js
@@ -0,0 +1,15 @@
+// Array Concatenation
+
+// Combine two or more arrays using the concat() method. Ir returns new array conaining joined arrays elements.
+
+// Creating an Array and Initializing with Values
+let courses = ["HTML", "CSS", "JavaScript", "React"];
+let otherCourses = ["Node.js", "Expess.js"];
+ 
+// Concatenate both arrays
+let concateArray = courses.concat(otherCourses);
+ 
+console.log("Concatenated Array: ", concateArray);
+
+// Output :- Concatenated Array:  [ 'HTML', 'CSS', 'JavaScript', 'React', 'Node.js', 'Expess.js' ]
+
