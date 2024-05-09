6981d74e23be242247c675cac32752d04ddd3724
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-09 23:57:50 +0530

    conversion

---
+++ 8-array/9-conversion.js
@@ -0,0 +1,13 @@
+// Conversion of an Array to String
+
+// We have a builtin method toString() to converts an array to a string.
+
+// Creating an Array and Initializing with Values
+let courses = ["HTML", "CSS", "JavaScript", "React"];
+ 
+// Convert array ot String
+console.log(courses.toString());
+
+// Output :-
+// HTML,CSS,JavaScript,React
+
