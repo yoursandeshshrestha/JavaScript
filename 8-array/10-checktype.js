740f0d77ca8c6cff0310a87105837b0bc9d8dd05
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-09 23:57:59 +0530

    checktype

---
+++ 8-array/10-checktype.js
@@ -0,0 +1,12 @@
+// Check the Type of an Arrays
+
+// The JavaScript typeof operator is used ot check the type of an array. It returns “object” for arrays.
+
+// Creating an Array and Initializing with Values
+let courses = ["HTML", "CSS", "JavaScript", "React"];
+ 
+// Check type of array
+console.log(typeof courses);
+
+// Output :- object
+
