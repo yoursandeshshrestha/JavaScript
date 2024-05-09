04491319e1b26a4d873e0c0a198fb4550aa1a497
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-09 23:57:06 +0530

    add element

---
+++ 8-array/4-addelement.js
@@ -0,0 +1,18 @@
+// Adding Elements to the Array
+
+// Elements can be added to the array using methods like push() and unshift().
+
+// Creating an Array and Initializing with Values
+let courses = ["HTML", "CSS", "Javascript", "React"];
+ 
+// Add Element to the end of Array
+courses.push("Node.js");
+ 
+// Add Element to the beginning
+courses.unshift("Web Development");
+ 
+console.log(courses);
+
+// Output
+// [ 'Web Development', 'HTML', 'CSS', 'Javascript', 'React', 'Node.js' ]
+
