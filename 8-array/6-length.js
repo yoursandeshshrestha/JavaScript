a6f1e804f4b2251731ba32bef76b198ba48e4d3c
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-09 23:57:19 +0530

    length

---
+++ 8-array/6-length.js
@@ -0,0 +1,13 @@
+// Array Length
+
+// Get the length of an array using the length property.
+
+// Creating an Array and Initializing with Values
+let courses = ["HTML", "CSS", "Javascript", "React", "Node.js"];
+ 
+let len = courses.length;
+ 
+console.log(`Array Length: ${len}`);
+
+// Output :- 
+// Array Length: 5
