d36c159c7e0b4e3c0f3e5e4c0381cb0c3046f8fd
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-09 23:56:44 +0530

    access

---
+++ 8-array/2-access.js
@@ -0,0 +1,12 @@
+// Accessing Elements of an Array
+
+// Any element in the array can be accessed using the index number. The index in the arrays starts with 0.
+
+// Creating an Array and Initializing with Values
+let courses = ["HTML", "CSS", "Javascript", "React"];
+ 
+// Accessing Array Elements
+console.log(courses[0]); // HTMl
+console.log(courses[1]); // CSS
+console.log(courses[2]); // Javascript
+console.log(courses[3]); // React
