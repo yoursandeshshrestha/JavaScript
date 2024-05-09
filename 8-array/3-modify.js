88837809d42aeaef7451af2612ee5b04bcc5ad97
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-09 23:56:54 +0530

    modify

---
+++ 8-array/3-modify.js
@@ -0,0 +1,14 @@
+// Modifying the Array Elements
+
+// Elements in an array can be modified by assigning a new value to their corresponding index.
+
+// Creating an Array and Initializing with Values
+let courses = ["HTML", "CSS", "Javascript", "React"];
+console.log(courses);
+ 
+courses[1]= "Bootstrap";
+console.log(courses);
+
+// Output 
+// [ 'HTML', 'CSS', 'Javascript', 'React' ]
+// [ 'HTML', 'Bootstrap', 'Javascript', 'React' ]
