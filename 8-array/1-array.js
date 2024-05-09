e7e46122e8eda3975204a86b1f5389eb6e8dc1e9
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-09 23:56:36 +0530

    array

---
+++ 8-array/1-array.js
@@ -0,0 +1,10 @@
+// Array: A data structure in JavaScript that allows you to store multiple values in a single variable.
+// Array Element: Each value within an array is called an element. Elements are accessed by their index.
+// Array Index: JavaScript arrays are zero-indexed, meaning the first element is at index 0.
+// Array Length: The number of elements in an array. It can be retrieved using the length property.
+
+// 1. Creating an Array using Array Literal
+let courses = ["HTML", "CSS", "Javascript", "React"];
+console.log(courses);
+
+// Output - [ 'HTML', 'CSS', 'Javascript', 'React' ]
