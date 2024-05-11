100bbcf6585ea357d81dc0ded291c60b869378ec
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-11 22:47:55 +0530

    pop

---
+++ 9-arraymethod/2.pop.js
@@ -0,0 +1,7 @@
+// pop(): Removes the last element from an array and returns that element.
+
+let fruits = ["Mango", "Orange", "Banana", "Kiwi"]
+
+let poppedFruit = fruits.pop();
+console.log(poppedFruit); // Output: "Kiwi"
+console.log(fruits); // Output: ["Mango", "Orange", "Banana"]
