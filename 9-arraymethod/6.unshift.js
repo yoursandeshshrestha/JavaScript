190fac67156aac5759f22df7f69d28d179fdb8e0
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-12 21:48:51 +0530

    unshift

---
+++ 9-arraymethod/6.unshift.js
@@ -0,0 +1,7 @@
+// unshift(): Adds one or more elements to the beginning of an array and returns the new length of the array.
+
+let fruits = ["Mango", "Orange", "Banana", "Kiwi"]
+
+let newLength = fruits.unshift("Apple", "Pear");
+console.log(newLength); // Output: 6
+console.log(fruits); // Output: ["Apple", "Pear", "Mango", "Pineapple", "Grapes", "Kiwi"]
