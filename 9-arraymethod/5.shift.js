badd2d8603c8a0155622d1c3be38fdc94e671e2e
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-12 21:48:44 +0530

    shift

---
+++ 9-arraymethod/5.shift.js
@@ -0,0 +1,7 @@
+// shift(): Removes the first element from an array and returns that removed element.
+
+let fruits = ["Mango", "Orange", "Banana", "Kiwi"]
+
+let shiftedFruit = fruits.shift();
+console.log(shiftedFruit); // Output: "Mango"
+console.log(fruits); // Output: ["Orange", "Banana", "Kiwi"]
