ddce39378e703b96af98bf46ca47c40afc02ae72
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-12 21:48:57 +0530

    slice

---
+++ 9-arraymethod/7.slice.js
@@ -0,0 +1,6 @@
+// slice(): Extracts a section of the array and returns a new array.
+
+let fruits = ["Mango", "Orange", "Banana", "Kiwi"]
+
+let slicedFruits = fruits.slice(1, 3); // one is starting and 2 is end
+console.log(slicedFruits); // Output: ["Orange", "Banana"]
