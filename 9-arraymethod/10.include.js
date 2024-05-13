86ab6e1736ad2a75fac7942eb0a0b3439776358a
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-13 19:07:31 +0530

    include

---
+++ 9-arraymethod/10.include.js
@@ -0,0 +1,8 @@
+let fruits = ["Mango", "Orange", "Banana", "Kiwi"]
+
+// includes(): Determines whether an array includes a certain element, 
+// returning true or false as appropriate.
+
+let hasMango = fruits.includes("Mango");
+console.log(hasMango); // Output: true
+
