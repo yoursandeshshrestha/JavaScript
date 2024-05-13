0341ce7ae840022bb2e07a950d4b245d745278a8
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-13 19:07:38 +0530

    indexof

---
+++ 9-arraymethod/11.indexof.js
@@ -0,0 +1,7 @@
+// indexOf(): Returns the first index at which a given element 
+// can be found in the array, or -1 if it is not present.
+
+let fruits = ["Mango", "Orange", "Banana", "Kiwi"]
+
+let bananaIndex = fruits.indexOf("Banana");
+console.log(bananaIndex); // Output: 2
