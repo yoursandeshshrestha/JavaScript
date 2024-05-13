4dc8061e41dd3477f163b4a438a1a43af2c57349
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-13 19:07:57 +0530

    join in

---
+++ 9-arraymethod/13.join.js
@@ -0,0 +1,9 @@
+// join(): Joins all elements of an array into a string.
+
+let fruits = ["Mango", "Orange", "Banana", "Kiwi"]
+
+let fruitsString = fruits.join(", ");
+console.log(fruitsString); // Output: "Mango, Orange, Banana, Kiwi"
+
+let anotherString = fruits.join("% ");
+console.log(anotherString); // Output: "Mango% Orange% Banana% Kiwi"
