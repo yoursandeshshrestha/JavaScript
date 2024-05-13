5d707497dd35d686e08c0972be9d77d6476f2ac0
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-13 19:07:49 +0530

    lastindexof

---
+++ 9-arraymethod/12.lastindexof.js
@@ -0,0 +1,7 @@
+// lastIndexOf(): Returns the last index at which a given element can 
+// be found in the array, or -1 if it is not present.
+
+let fruits = ["Mango", "Orange", "Banana", "Kiwi"]
+
+let bananaIndex = fruits.lastIndexOf("Banana");
+console.log(bananaIndex); // Output: 2
