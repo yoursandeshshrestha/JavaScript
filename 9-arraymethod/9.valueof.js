6abd71825ab1e0fb2b0ed9ff3d499d501be52dcc
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-13 19:07:24 +0530

    valueof

---
+++ 9-arraymethod/9.valueof.js
@@ -0,0 +1,6 @@
+// valueOf(): Returns the primitive value of the specified object (used by toString()).
+
+let fruits = ["Mango", "Orange", "Banana", "Kiwi"]
+
+let primitiveValue = fruits.valueOf();
+console.log(primitiveValue); [ 'Mango', 'Orange', 'Banana', 'Kiwi' ]
