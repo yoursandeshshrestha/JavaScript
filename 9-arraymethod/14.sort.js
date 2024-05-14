b7115363a7e4a63ff79b5d213f7091b138dd0ca6
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-14 23:08:55 +0530

    sort

---
+++ 9-arraymethod/14.sort.js
@@ -0,0 +1,7 @@
+// sort(): Sorts the elements of an array in place and returns the sorted array.
+
+let fruits = ["Mango", "Orange", "Banana", "Kiwi"]
+
+let sortedFruits = fruits.sort();
+console.log(sortedFruits); // Output: ["Banana", "Kiwi", "Mango", "Orange"]
+// sort in alphabetic order
