00854651d9d28ca755f9c8333fe8c9f18c12bf7f
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-14 23:09:02 +0530

    tostring

---
+++ 9-arraymethod/15.tostring.js
@@ -0,0 +1,6 @@
+// toString(): Returns a string representing the specified array and its elements.
+
+let fruits = ["Mango", "Orange", "Banana", "Kiwi"]
+
+let fruitsString = fruits.toString();
+console.log(fruitsString); // Output: "Mango,Pineapple,Grapes,Kiwi"
