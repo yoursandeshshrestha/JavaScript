4a190a53a9c909b54d65fe20844d29a5f93de340
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-17 23:03:31 +0530

    require

---
+++ 12-Json/8-require.js
@@ -0,0 +1,9 @@
+// This method require Node Js
+// use fetch and catch method
+
+const data = require('./sample.json');
+console.log(data);
+console.log(data.users);
+
+console.log(data.users[0].site); // GeeksforGeeks
+console.log(data.users[0].user); // Shobhit
