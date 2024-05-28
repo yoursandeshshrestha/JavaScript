9bf84c8f2689f24ae4fd0752eb40f43b7f71e3e1
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-05-28 11:54:19 +0530

    global scope

---
+++ 17-scope/1.global.js
@@ -0,0 +1,17 @@
+// Global Scope:
+// The global scope in JavaScript refers to the outermost scope where variables 
+// and functions are accessible from anywhere within the codebase.
+// Variables and functions declared in the global scope are accessible 
+// throughout the entire JavaScript program.
+
+Example:
+
+// Variables declared in the global scope
+var globalVar = "I am a global variable";
+
+function globalFunction() {
+console.log("I am a global function");
+}
+
+console.log(globalVar); // Output: I am a global variable
+globalFunction(); // Output: I am a global function
